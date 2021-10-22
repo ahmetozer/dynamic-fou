80b7040784ca36940e5f1ffab31df72558adef02
ahmetozer <git@ahmetozer.org>
2021-10-23 02:47:46 +0300

    fix: UDPPortCheck
    converted to UDP

--- share/port_check.go
+++ share/port_check.go
@@ -3,19 +3,22 @@ package share
 import (
 	"fmt"
 	"net"
-	"strconv"
 )
 
-func UDPPortCheck(port uint16) error {
-	tempPort, err := net.Listen("udp", ":"+strconv.FormatUint(uint64(port), 10))
+func UDPPortCheck(port int) error {
+
+	tempPort, err := net.ListenUDP("udp", &net.UDPAddr{
+		Port: port,
+		IP:   net.ParseIP("::"),
+	})
 
 	if err != nil {
-		return fmt.Errorf("can't listen on port %q: %s", port, err)
+		return fmt.Errorf("can't listen on port %v, %s", port, err)
 	}
 
 	err = tempPort.Close()
 	if err != nil {
-		return fmt.Errorf("couldn't stop listening on port %q: %s", port, err)
+		return fmt.Errorf("couldn't stop listening on port %v, %s", port, err)
 	}
 	return nil
 }
